21dc6939bd6e555e159d175338a32163fb98f3ef
Jacqueline Buros <jburos@penguinrandomhouse.com>
2017-12-18 12:45:10 -0500

    update rcctils expression data to include sampleinfo in object attributes

--- data-raw/rcctils_expression_matrix.R
+++ data-raw/rcctils_expression_matrix.R
@@ -1,7 +1,19 @@
 library(rinfino)
 library(devtools)
 
-rccutils_expression <- rinfino::load_expdata(system.file("testdata",
+rcctils_expression <- rinfino::load_expdata(system.file("testdata",
                                         "test_expression_matrix.tsv.gz",
                                         package="rinfino"))
-devtools::use_data(rcctils_expression, compress = 'xz')
+
+rcctils_sampleinfo <- readr::read_tsv(system.file('data-raw', 'E-MTAB-5640.sdrf.txt', package = 'rinfino')) %>%
+  dplyr::mutate(sample_id = `Comment[ENA_RUN]`,
+                filename = basename(`Comment[FASTQ_URI]`),
+                sample_description = stringr::str_c(`Characteristics[phenotype]`,
+                                                    `Characteristics[cell type]`,
+                                                    sep = '-'))
+
+rcctils_expression <- rinfino::update_sampleinfo(expdata = rcctils_expression,
+                           df = rcctils_sampleinfo,
+                           sample_id = 'sample_id')
+
+devtools::use_data(rcctils_expression, compress = 'xz', overwrite = T)
